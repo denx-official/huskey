e933f5b8b4c7da168e26ffbef18f182d8a39fa47
ippee <ippee.music@gmail.com>
2021-05-04 11:27:15 +0900

    56a792a をpwshにも対応

--- bin/huskey.ps1
+++ bin/huskey.ps1
@@ -1,4 +1,10 @@
-$ps1_file = which hello
-$bin_dir = Split-Path $ps1_file
+if (gcm java -ea SilentlyContinue) { # java コマンドが存在する場合
+  $ps1_file = which huskey
+  $bin_dir = Split-Path $ps1_file
 
-java -classpath $bin_dir Huskey $Args
+  java -classpath $bin_dir Huskey $Args
+} else {
+  echo "huskey を使用するには The Java Runtime Environment (JRE) をインストールする必要があります。"
+  echo "Java | Oracle (https://java.com/) より最新版の JRE をインストールしてください。"
+  exit 1
+}
