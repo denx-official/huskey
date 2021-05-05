09e048d7e66fceb4c595db70167bca8f0114b57f
ippee <ippee.music@gmail.com>
2021-05-05 13:52:46 +0900

    getメソッドの追加

--- src/main/huskey/utility/HuskeyArgs.java
+++ src/main/huskey/utility/HuskeyArgs.java
@@ -1,13 +1,25 @@
 package utility;
 
 public class HuskeyArgs {
-    String command;
-    String[] values;
-    String[] options;
+    private String command;
+    private String[] values;
+    private String[] options;
 
     public HuskeyArgs(String command, String[] values, String[] options) {
         this.command = command;
         this.values = values;
         this.options = options;
+    }
+
+    public String getCommand() {
+        return this.command;
+    }
+
+    public String[] getValues() {
+        return this.values;
+    }
+
+    public String[] getOptions() {
+        return this.options;
     }
 }
