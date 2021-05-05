9ddd20e29c98155b65b2676ee003ab421794f26d
ippee <ippee.music@gmail.com>
2021-05-05 13:04:25 +0900

    コマンドライン引数分割と文法解析を分離するためのクラスを作成

---
+++ src/test/huskey/utility/CheckArgsTest.java
@@ -0,0 +1,27 @@
+package utility;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckArgsTest {
+    private String command;
+    private String[] values;
+    private String[] options;
+
+    @Nested
+    class CommandTest {
+
+    }
+
+    @Nested
+    class ValuesTest {
+
+    }
+
+    @Nested
+    class OptionsTest {
+
+    }
+}
