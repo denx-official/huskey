fac86e365487437adac5d06a07b0e5f91cb1b296
ippee <ippee.music@gmail.com>
2021-05-05 11:36:39 +0900

    helpコマンドの追加

---
+++ src/main/huskey/cmd/Help.java
@@ -0,0 +1,17 @@
+package cmd;
+
+public class Help extends Cmd {
+    public Help(String[] values, String[] options) {
+        super(values, options);
+    }
+
+    public void run() {
+        // ここにコマンドの機能を実装する
+        String values_str = String.join(", ", this.values);
+        String options_str = String.join(", ", this.options);
+
+        System.out.println("run help");
+        System.out.println("values: " + values_str);
+        System.out.println("options: " + options_str);
+    }
+}
--- src/main/huskey/utility/CommandsList.java
+++ src/main/huskey/utility/CommandsList.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class CommandsList {
     private String[] commandsList = {
+        "help",
         "init",
         "change",
         "database",
