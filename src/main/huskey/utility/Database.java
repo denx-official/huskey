9257184fb5e32482a5536745cd2a139e7c045454
ippee <ippee.music@gmail.com>
2021-06-25 14:42:18 +0900

    getDatasetの仮実装

--- src/main/huskey/utility/Database.java
+++ src/main/huskey/utility/Database.java
@@ -1,6 +1,11 @@
 package utility;
 
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.FileNotFoundException;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public class Database {
@@ -18,5 +23,16 @@ public class Database {
 
     public boolean isKeyMatched() throws FileNotFoundException {
         return Objects.equals(this.masterKey, "sample");
+    }
+
+    public Document getDataset() throws Exception {
+        try {
+            String path = Database.class.getClassLoader().getResources(this.dbName).nextElement().toString();
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            return builder.parse(Paths.get(path.replace("file:/", "")).toFile());
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
