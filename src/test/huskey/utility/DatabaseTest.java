15fc84d8a069184e24d86fe62bd053a3518d729a
ippee <ippee.music@gmail.com>
2021-06-25 14:39:49 +0900

    getDatasetのテスト追加

--- src/test/huskey/utility/DatabaseTest.java
+++ src/test/huskey/utility/DatabaseTest.java
@@ -1,9 +1,16 @@
 package utility;
 
 import org.junit.jupiter.api.*;
+import org.w3c.dom.Document;
 
-import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.FileNotFoundException;
+import java.io.StringWriter;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,6 +36,38 @@ class DatabaseTest {
         void masterKeyの照合_false() throws FileNotFoundException {
             db = new Database("sample", "sanple");
             assertFalse(db.isKeyMatched());
+        }
+    }
+
+    @Nested
+    class getDataset {
+        @Test
+        void データセットの取得() throws Exception {
+            db = new Database("sample", "sample");
+            String result = xmlToString(db.getDataset());
+            String expect = xmlToString(createExpectDoc());
+            assertEquals(expect, result);
+        }
+
+        Document createExpectDoc() throws Exception {
+            try {
+                String path = Database.class.getClassLoader().getResources("expect.xml").nextElement().toString();
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                return builder.parse(Paths.get(path.replace("file:/", "")).toFile());
+            } catch (Exception e) {
+                throw new Exception(e.getMessage());
+            }
+        }
+
+        String xmlToString(Document doc) throws TransformerException {
+            StringWriter writer = new StringWriter();
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty("{http://xml.apache.org/xalan}indent-amount", "2");
+            transformer.transform(new DOMSource(doc), new StreamResult(writer));
+            return writer.toString();
         }
     }
 
