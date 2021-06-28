3bb857efd2ffcbdeb410a3a14b3f08fe345b42ef
ippee <ippee.music@gmail.com>
2021-06-29 08:51:58 +0900

    javaコマンドの存在を判定するコードの修正

--- bin/huskey.ps1
+++ bin/huskey.ps1
@@ -1,10 +1,11 @@
-if (gcm java -ea SilentlyContinue) { # java コマンドが存在する場合
-  $ps1_file = which huskey
-  $bin_dir = Split-Path $ps1_file
+$ErrorActionPreference = "Stop"
 
-  java -classpath $bin_dir Huskey $Args
-} else {
-  echo "huskey を使用するには The Java Runtime Environment (JRE) をインストールする必要があります。"
-  echo "Java | Oracle (https://java.com/) より最新版の JRE をインストールしてください。"
-  exit 1
+if (!(gcm java -ea SilentlyContinue)) { # java コマンドが存在しない場合
+    echo "huskey を使用するには The Java Runtime Environment (JRE) をインストールする必要があります。"
+    echo "Java | Oracle (https://java.com/) より最新版の JRE をインストールしてください。"
+    exit 1
 }
+
+$ps1_file = which huskey
+$bin_dir = Split-Path $ps1_file
+java -classpath $bin_dir Huskey $Args
