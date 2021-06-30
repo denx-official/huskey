57536d5547603afbf1a12a562816c596fcf7d810
ippee <ippee.music@gmail.com>
2021-06-30 19:33:14 +0900

    Datasetを取得するメソッドの追加

--- src/main/huskey/database/Database.java
+++ src/main/huskey/database/Database.java
@@ -1,6 +1,9 @@
 package database;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.xpath.*;
 
 public class Database {
     private Document doc;
@@ -11,5 +14,16 @@ public class Database {
         this.doc = doc;
         this.dbName = dbName;
         this.masterKey = masterKey;
+    }
+
+    public Dataset useDataset() {
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        try {
+            XPathExpression expr = xpath.compile("/database/dataset");
+            NodeList nodeList = (NodeList) expr.evaluate(this.doc, XPathConstants.NODESET);
+            return new Dataset(nodeList);
+        } catch (XPathExpressionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/huskey/database/Dataset.java
+++ src/main/huskey/database/Dataset.java
@@ -1,4 +1,11 @@
 package database;
 
+import org.w3c.dom.NodeList;
+
 public class Dataset {
+    private NodeList nodeList;
+
+    public Dataset(NodeList nodeList) {
+        this.nodeList = nodeList;
+    }
 }
