00bbb7ebe107107cd7421ad6b858377e90a7265c
ippee <ippee.music@gmail.com>
2021-06-30 20:11:14 +0900

    useDataの実装

--- src/main/huskey/database/Dataset.java
+++ src/main/huskey/database/Dataset.java
@@ -1,7 +1,10 @@
 package database;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import java.util.Objects;
 
 public class Dataset {
     private NodeList nodeList;
@@ -17,5 +20,41 @@ public class Dataset {
             dataList[i] = data.getAttribute("title");
         }
         return dataList;
+    }
+
+    public Data useData(String target) throws IllegalArgumentException {
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Element data = (Element) nodeList.item(i);
+            String title = data.getAttribute("title");
+            if (Objects.equals(title, target)) {
+                return createData(data);
+            }
+        }
+
+        throw new IllegalArgumentException(target + " の名前を持つデータが存在しません。");
+    }
+
+    private Data createData(Element data) {
+        Element createdElem = (Element) data.getElementsByTagName("created").item(0);
+        Element updatedElem = (Element) data.getElementsByTagName("updated").item(0);
+        return new Data(
+            data.getAttribute("title"),
+            data.getElementsByTagName("userName").item(0).getTextContent(),
+            data.getElementsByTagName("password").item(0).getTextContent(),
+            data.getElementsByTagName("message").item(0).getTextContent(),
+            elementToHkTime(createdElem),
+            elementToHkTime(updatedElem)
+        );
+    }
+
+    private HkTime elementToHkTime(Element elem) {
+        return new HkTime(
+            Integer.parseInt(elem.getAttribute("year")),
+            Integer.parseInt(elem.getAttribute("month")),
+            Integer.parseInt(elem.getAttribute("date")),
+            Integer.parseInt(elem.getAttribute("hours")),
+            Integer.parseInt(elem.getAttribute("minutes")),
+            Integer.parseInt(elem.getAttribute("seconds"))
+        );
     }
 }
