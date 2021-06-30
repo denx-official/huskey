26c9c4bd7c31301e0b4ef73d5d3e23a590fe6586
ippee <ippee.music@gmail.com>
2021-06-30 21:49:58 +0900

    データを更新する機能の実装

--- src/main/huskey/database/Dataset.java
+++ src/main/huskey/database/Dataset.java
@@ -50,6 +50,33 @@ public class Dataset {
         throw new IllegalArgumentException(target + " の名前を持つデータが存在しません。");
     }
 
+    public void setData(String target, Data data) {
+        NodeList nodeList = this.root.getChildNodes();
+        for (int i = nodeList.getLength() - 1; i >= 0; i--) {
+            Element elem = (Element) nodeList.item(i);
+            String title = elem.getAttribute("title");
+            if (Objects.equals(title, target)) {
+                updateDataset(elem, data);
+            }
+        }
+    }
+
+    private void updateDataset(Element elem, Data data) {
+        elem.setAttribute("title", data.title());
+        elem.getElementsByTagName("userName").item(0).setTextContent(data.userName());
+        elem.getElementsByTagName("password").item(0).setTextContent(data.password());
+        elem.getElementsByTagName("message").item(0).setTextContent(data.message());
+        Element updatedElem = (Element) elem.getElementsByTagName("updated").item(0);
+
+        HkTime updated = data.updated();
+        updatedElem.setAttribute("year", String.valueOf(updated.year));
+        updatedElem.setAttribute("month", String.valueOf(updated.month));
+        updatedElem.setAttribute("date", String.valueOf(updated.date));
+        updatedElem.setAttribute("hours", String.valueOf(updated.hours));
+        updatedElem.setAttribute("minutes", String.valueOf(updated.minutes));
+        updatedElem.setAttribute("seconds", String.valueOf(updated.seconds));
+    }
+
     private Data createData(Element data) {
         Element createdElem = (Element) data.getElementsByTagName("created").item(0);
         Element updatedElem = (Element) data.getElementsByTagName("updated").item(0);
