3c9444b95eafcd6e3597117cf08f858b7196f077
ippee <ippee.music@gmail.com>
2021-07-03 14:21:28 +0900

    javadocの追加

--- src/main/huskey/utility/HuskeyException.java
+++ src/main/huskey/utility/HuskeyException.java
@@ -1,5 +1,13 @@
 package utility;
 
+/**
+ * huskey実行時に発生しうる例外クラス
+ *
+ * <p>この例外は cmd.CommandRouting クラスの run メソッドで捕捉され、与えられたメッセージが標準エラーとして表示される。
+ *
+ * @see cmd.CommandRouting
+ * @author いっぺー
+ */
 public class HuskeyException extends RuntimeException {
     public HuskeyException(String message) {
         super(message);
