c595a331d099c5525b732226bea6664f16571e12
ippee <ippee.music@gmail.com>
2021-07-04 21:07:43 +0900

    DBChildクラスの追加

---
+++ src/main/huskey/database/DBChild.java
@@ -0,0 +1,14 @@
+package database;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+public abstract class DBChild {
+    protected final Document doc;
+    protected final Node root;
+
+    DBChild(Document doc, Node root) {
+        this.doc = doc;
+        this.root = root;
+    }
+}
