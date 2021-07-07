a905f8372c6500ab30035c9ea00b6e3625c606cb
ippee <ippee.music@gmail.com>
2021-07-07 18:32:15 +0900

    ConfTestの追加

---
+++ src/test/huskey/database/conf/ConfTest.java
@@ -0,0 +1,32 @@
+package database.conf;
+
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Node;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfTest {
+    private Conf conf;
+    private ConfBuilder builder;
+    private final String huskeyDir = "./target/test-classes/resources/";
+
+    @Test
+    void globalConfの読み込み() {
+        builder = new ConfBuilder(ConfType.globalConf, huskeyDir);
+        conf = builder.build();
+
+        Node node = conf.searchNodeList("//defaultDB").item(0);
+        String result = node.getTextContent();
+        assertEquals("SampleDB", result);
+    }
+
+    @Test
+    void initialConfの読み込み() {
+        builder = new ConfBuilder(ConfType.initialConf, huskeyDir);
+        conf = builder.build();
+
+        Node node = conf.searchNodeList("//passLength").item(0);
+        String result = node.getTextContent();
+        assertEquals("16", result);
+    }
+}
