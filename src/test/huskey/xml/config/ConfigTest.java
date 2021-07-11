dda159ee2b1dce2feef6e20ce50cf7c7952f3586
ippee <ippee.music@gmail.com>
2021-07-12 00:28:49 +0900

    18378c5 に際してテストの修正

--- src/test/huskey/xml/config/ConfigTest.java
+++ src/test/huskey/xml/config/ConfigTest.java
@@ -1,5 +1,6 @@
 package xml.config;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Node;
 import xml.StaticXMLMethods;
@@ -8,15 +9,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ConfigTest {
     private Config conf;
-    private ConfigBuilder builder;
     private final String huskeyDir = "./target/classes/";
+
+    @BeforeEach
+    void setup() {
+        ConfigBuilder builder = new ConfigBuilder(huskeyDir);
+        conf = builder.build();
+    }
 
     @Test
     void configファイルの読み込み() {
-        builder = new ConfigBuilder(huskeyDir);
-        conf = builder.build();
-
-        Node node = StaticXMLMethods.searchNodeList(conf.doc, "//defaultDB").item(0);
+        Node node = StaticXMLMethods.searchNodeList(conf.getDoc(), "//defaultDB").item(0);
         String result = node.getTextContent();
         assertEquals("SampleDB", result);
     }
