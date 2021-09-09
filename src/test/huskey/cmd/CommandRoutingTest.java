45a18bb4163898f6e546c385d601e81eae3f2e83
ippee <ippee.music@gmail.com>
2021-09-10 01:15:33 +0900

    29ef049 によるテストの修正

--- src/test/huskey/cmd/CommandRoutingTest.java
+++ src/test/huskey/cmd/CommandRoutingTest.java
@@ -2,19 +2,17 @@ package cmd;
 
 import org.junit.jupiter.api.Test;
 import utility.HuskeyException;
-import utility.SeparateArgs;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommandRoutingTest {
     @Test
     void 適切なコマンドが与えられなかった場合() {
-        String[] args = {"hoge", "fuga"};
-        SeparateArgs sepArgs = new SeparateArgs(args);
         CommandRouting cr = new CommandRouting(
-                sepArgs.getCommand(),
-                sepArgs.getValues(),
-                sepArgs.getOptions()
+                "hoge",
+                new String[]{""},
+                new String[]{""},
+                null
         );
         assertThrows(HuskeyException.class, cr::_run);
     }
