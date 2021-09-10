3c4557339d94dde30a5eb62ddce19d5fdf1f07af
ippee <ippee.music@gmail.com>
2021-09-10 16:14:47 +0900

    引数をhkArgs -> dbNameに変更
    メソッド内でhkArgsはデータベース名の生成処理しか行っていなかったため、メソッドの外で生成し、引数として渡したほうが柔軟性が増す

--- src/main/huskey/cmd/UseDatabase.java
+++ src/main/huskey/cmd/UseDatabase.java
@@ -1,24 +1,36 @@
 package cmd;
 
-import args.HkArgs;
 import utility.HiddenInput;
 import utility.HuskeyRuntimeException;
 import xml.database.Database;
 import xml.database.DatabaseBuilder;
 
+/**
+ * データベースの取得を行うクラス
+ *
+ * @author いっぺー
+ */
 public class UseDatabase {
-    public static Database useDB(HkArgs hkArgs, String huskeyDir) {
-        String dbName = hkArgs.dbName(huskeyDir);
+    /**
+     * データベースの取得
+     *
+     * <p>masterKey の取得 -> 照合をする処理や、データベースの存在確認といった、
+     * 各コマンドで共通するフローをまとめた static method。
+     *
+     * @param dbName データベース名
+     * @param huskeyDir ドットフォルダーのディレクトリ
+     * @return Database
+     */
+    public static Database useDB(String dbName, String huskeyDir) {
         String masterKey = HiddenInput.read("データベース " + dbName + " のパスワード: ");
-
         DatabaseBuilder builder = new DatabaseBuilder(dbName, masterKey, huskeyDir);
 
         if (!builder.exists()) {
-            throw new HuskeyRuntimeException("huskey: データベース '" + builder.dbName + "' は存在しません。");
+            throw new HuskeyRuntimeException("huskey: データベース '" + dbName + "' は存在しません。");
         }
 
         if (!builder.isKeyMatched()) {
-            throw new HuskeyRuntimeException("huskey: データベース '" + builder.dbName + "' の masterKey が不正です。");
+            throw new HuskeyRuntimeException("huskey: データベース '" + dbName + "' の masterKey が不正です。");
         }
 
         return builder.build();
